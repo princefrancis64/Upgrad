b7d4c58df82eff33571245dffd0088690ae80b02
princefrancis64 <prince.francis64@gmail.com>
2024-04-22 18:38:29 +0530

    1 hour completed

---
+++ Python/hello.py
@@ -0,0 +1,27 @@
+# Ask user for their name  
+# name = input("What's your name?")
+
+# Say hello to user
+# print("hello",name)
+# print("hello,"+name)
+# print("hello",end=' ')
+# print(name)
+# print("hello,",name,sep="??????")
+# print('hello, "friend"')
+# print("hello,\"friend\"")
+
+#Remove whitespace from str
+# name = name.strip()
+#Capitalize the user's name
+# name = name.capitalize()
+
+# name = name.title()
+
+# Remove whitespaces from str and capitalize the user's name
+# name  = name.strip().title()
+
+name  = input("What's your name?").strip().title()
+print(f"hello,{name}") # fstring
+
+
+
