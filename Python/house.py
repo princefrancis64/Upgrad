72f9a0d81e87c91556d73d71becd9af6de04fcfe
princefrancis64 <prince.francis64@gmail.com>
2024-04-22 22:11:21 +0530

    house.py

---
+++ Python/house.py
@@ -0,0 +1,33 @@
+name = input("What's your name? ")
+
+# if name=="Harry":
+#     print("Gryffindor")
+# elif name=="Hermione":
+#     print("Griffindor")
+# elif name =="Ron":
+#     print("Griffindor")
+# elif name=="Draco":
+#     print("Slytherin")
+# else:
+#     print("Who?")
+
+# match name:
+#     case "Harry":
+#         print("Gryffindor")
+#     case "Hermione":
+#         print("Gryffindor")
+#     case "Ron":
+#         print("Gryffindor")
+#     case "Draco":
+#         print("Slytherin")
+#     case _:
+#         print("Who?")
+
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
