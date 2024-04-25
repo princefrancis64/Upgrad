804ea24762e3a612e58ec0de6b16dcd047bc58a5
princefrancis64 <prince.francis64@gmail.com>
2024-04-25 14:51:03 +0530

    12 hours completed

--- Python/student.py
+++ Python/student.py
@@ -31,11 +31,11 @@
 #     return [name,house]
 
 
-def main():
-    student = get_student()
-    if student['name']=="Padma":
-        student['house'] = "Ravenclaw"
-    print(f"{student['name']} is from {student['house']}")
+# def main():
+#     student = get_student()
+#     if student['name']=="Padma":
+#         student['house'] = "Ravenclaw"
+#     print(f"{student['name']} is from {student['house']}")
 
 # def get_student():
 #     student = {}
@@ -44,10 +44,126 @@ def main():
 #     return student
 
 
+# def get_student():
+#     name = input("Name: ")
+#     house = input("House: ")
+#     return {"name":name,"house":house}
+
+# class Student:
+#     ...
+
+
+# def main():
+#     student = get_student()
+#     print(f"{student.name} is from {student.house}")
+
+# def get_student():
+#     student = Student()
+#     student.name = input("Name: ")
+#     student.house = input("House: ")
+#     return student
+
+
+# class Student:
+#     def __init__(self,name,house):
+#         if not name:
+#             raise ValueError("Missing name")
+#         if house not in ["Gryffindor","Hufflepuff","Ravenclaw","Slytherin"]:
+#             raise ValueError("Invalid house")
+#         self.name = NameError
+#         self.house = house
+
+
+# def main():
+#     student = get_student()
+#     print(f"{student.name} is from {student.house}")
+
+# def get_student():
+#     name = input("Name: ")
+#     house = input("House: ")
+#     # student  = Student(name, house)
+#     return Student(name, house)
+
+#     # return student
+
+
+# class Student:
+#     ...
+
+
+# def main():
+#     student = get_student()
+#     print(f"{student.name} is from {student.house}")
+
+# def get_student():
+#     student = Student()
+#     student.name = input("Name: ")
+#     student.house = input("House: ")
+#     return student
+
+
+# class Student:
+#     def __init__(self,name,house,patronus):
+#         if not name:
+#             raise ValueError("Missing Name")
+#         if house not in ["Gryffindor","Hufflepuff","Ravenclaw","Slytherin"]:
+#             raise ValueError("Invalid house")
+#         self.name = name
+#         self.house = house
+#         self.patronus = patronus
+
+#     def __str__(self):
+#         return f"{self.name} is from {self.house}"
+    
+
+#     def charm(self):
+#         match self.patronus:
+#             case "Stag":
+#                 return "🐎"
+#             case "Otter":
+#                 return "🦦"
+#             case "Jack Russell terrier":
+#                 return "🐶"
+#             case _:
+#                 return "🧙"
+
+
+# def main():
+#     student = get_student()
+#     # print(f"{student.name} is from {student.house}")
+#     print("Expecto Patronum!")
+#     print(student.charm())
+    
+
+# def get_student():
+#     name  = input("Name: ")
+#     house  = input("House: ")
+#     patronus = input("Patronous: ")
+#     # student = Student(name,house)
+#     return Student(name,house,patronus)
+
+
+class Student:
+    def __init__(self,name,house):
+        if not name:
+            raise ValueError("Missing Name")
+        if house not in ["Gryffindor","Hufflepuff","Ravenclaw","Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+    
+    def __str__(self):
+        return f"{self.name} is from {self.house}"
+
+def main():
+    student = get_student()
+    student.house = "Number Four,Private Drive"
+    print(student)
+
 def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return {"name":name,"house":house}
+    name  = input("Name: ")
+    house  = input("House: ")
+    return Student(name,house)
 
 if __name__=="__main__":
     main()
