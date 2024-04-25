d58fe2a9bbb8026c2cde65ea0400eccc24505a7e
princefrancis64 <prince.francis64@gmail.com>
2024-04-25 16:15:37 +0530

    instance method

--- Python/student.py
+++ Python/student.py
@@ -143,27 +143,51 @@
 #     return Student(name,house,patronus)
 
 
-class Student:
-    def __init__(self,name,house):
-        if not name:
-            raise ValueError("Missing Name")
-        if house not in ["Gryffindor","Hufflepuff","Ravenclaw","Slytherin"]:
-            raise ValueError("Invalid house")
-        self.name = name
-        self.house = house
+# class Student:
+#     def __init__(self,name,house):
+#         self.name = name
+#         self.house = house
     
-    def __str__(self):
-        return f"{self.name} is from {self.house}"
+#     def __str__(self):
+#         return f"{self.name} is from {self.house}"
+    
 
-def main():
-    student = get_student()
-    student.house = "Number Four,Private Drive"
-    print(student)
+#     @property
+#     def name(self):
+#         return self._name
+    
+#     @name.setter
+#     def name(self,name):
+#         if not name:
+#             raise ValueError("Missing Name")
+#         self._name = name
+    
+#     #Getter
+#     @property
+#     def house(self):
+#         return self._house
+    
+#     # Setter
+#     @house.setter
+#     def house(self,house):
+#         if house not in ["Gryffindor","Hufflepuff","Ravenclaw","Slytherin"]:
+#             raise ValueError("Invalid house")
+#         self._house= house
 
-def get_student():
-    name  = input("Name: ")
-    house  = input("House: ")
-    return Student(name,house)
+# def main():
+#     student = get_student()
+#     # student.house = "Number Four,Private Drive"
+#     student._house = "Number Four,Privet Drive"
+#     print(student)
 
-if __name__=="__main__":
-    main()
+# def get_student():
+#     name  = input("Name: ")
+#     house  = input("House: ")
+#     return Student(name,house)
+
+# if __name__=="__main__":
+#     main()
+
+
+
+print(type(50))
