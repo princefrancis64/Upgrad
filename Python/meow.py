dbc723acbc4d950e8c93f830d5308d08fe56ceeb
princefrancis64 <prince.francis64@gmail.com>
2024-04-25 20:03:39 +0530

    14 hours completed

---
+++ Python/meow.py
@@ -0,0 +1,17 @@
+# MEOWS = 3
+
+# MEOWS = 4
+# for _ in range(MEOWS):
+#     print("meow")
+
+
+class Cat:
+    MEOWS=3
+
+    def meow(self,):
+        for _ in range(Cat.MEOWS):
+            print("meow")
+
+
+cat = Cat()
+cat.meow()
