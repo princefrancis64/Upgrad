dc3796027246cab2134f0ce37eabb45c67ab1fee
princefrancis64 <prince.francis64@gmail.com>
2024-05-13 18:03:12 +0530

    triggers added

--- Sql/DML_commands.sql
+++ Sql/DML_commands.sql
@@ -647,3 +647,135 @@ where Product_Category = 'technology';
 
 
 -- 3. Display the number of products sold by each manufacturer
+
+-- --------------------------------------------------------------------------------------------------------------------------------
+USE market_star_schema;
+
+CREATE TABLE employees(
+	`employee_id` VARCHAR(2) NOT NULL,
+    `first_name` VARCHAR(30) NOT NULL,
+    `last_name` VARCHAR(30) NOT NULL,
+    `hourly_pay` DECIMAL(10,2) NOT NULL,
+    `job` VARCHAR(30) NOT NULL,
+    `hire_date` DATE NOT NULL,
+    `supervisor_id` VARCHAR(2) DEFAULT NULL
+);
+
+
+-- Inserting values into the table employees
+INSERT INTO `employees` VALUES
+('1','Eugene','Krabs',25.50,'Manager','2023-01-02',NULL),
+('2','Squidward','Tentacles',15.00,'cashier','2023-01-03','5'),
+('3','Spongebob','Squarepants',12.50,'cook','2023-01-04','5'),
+('4','Patrick','Star',12.50,'cook','2023-01-05','5'),
+('5','Sandy','Cheeks',17.25,'asst. manager','2023-01-06','1'),
+('6','Sheldon','Plankton',10.00,'janitor','2023-01-07','5');
+
+
+SELECT * FROM employees;
+
+
+-- Adding salary column after the hourly pay
+ALTER TABLE employees
+ADD COLUMN salary DECIMAL(10,2) AFTER hourly_pay;
+
+-- Updating the salary column
+UPDATE employees
+SET salary = hourly_pay*2080;
+
+
+-- So here we will create a trigger so that salary gets updated automatically when hourly_pay is updated
+CREATE TRIGGER before_hourly_pay_update
+BEFORE UPDATE ON employees
+FOR EACH ROW
+SET NEW.salary=(NEW.hourly_pay * 2080);
+
+
+-- to show triggers
+SHOW TRIGGERS;
+
+UPDATE employees
+SET hourly_pay=50
+WHERE employee_id='1';
+
+SELECT * FROM employees;
+
+UPDATE employees
+SET hourly_pay=hourly_pay+1;
+
+-- Now we will create a trigger which kicks in when a new hourly_pay is added
+DELETE FROM employees
+WHERE employee_id='6';
+
+CREATE TRIGGER before_hourly_pay_insert
+BEFORE INSERT on employees
+FOR EACH ROW
+SET NEW.salary = (NEW.hourly_pay*2080);
+
+INSERT INTO employees VALUES
+('6','Sheldon','Plankton',10,NULL,'janitor','2023-01-07','5');
+SELECT * FROM employees;
+
+-- CREATING A NEW TABLE
+CREATE TABLE expenses(
+	expense_id INT PRIMARY KEY,
+    expense_name VARCHAR(50),
+    expense_total DECIMAL(10,2)
+    );
+    
+    
+INSERT INTO expenses VALUES
+(1,'salaries',0),
+(2,'supplies',0),
+(3,'taxes',0);
+
+SELECT * FROM expenses;
+
+UPDATE expenses
+SET expense_total = (SELECT SUM(salary) FROM employees)
+WHERE expense_name ='salaries'; 
+
+CREATE TRIGGER after_salary_delete
+AFTER DELETE ON employees
+FOR EACH ROW
+UPDATE expenses
+SET expense_total=expense_total - OLD.salary
+WHERE expense_name='salaries';
+
+
+DELETE FROM employees
+WHERE employee_id='6';
+
+
+-- Create a trigger which updates the salary whenever we add a new employee
+DROP TRIGGER after_salary_insert;
+
+CREATE TRIGGER after_salary_insert
+AFTER INSERT ON employees
+FOR EACH ROW
+UPDATE expenses
+SET expense_total = expense_total+NEW.salary
+WHERE expense_name = 'salaries';
+
+INSERT INTO employees
+VALUES('6','Sheldon','Plankton',10,NULL,'janitor','2023-01-07','5');
+
+
+-- Creating a trigger which will update the expense_total when we update the salary
+DROP TRIGGER after_salary_update;
+
+CREATE TRIGGER after_salary_update
+AFTER UPDATE ON employees
+FOR EACH ROW
+UPDATE expenses
+SET expense_total=expense_total+(NEW.salary-OLD.salary)
+WHERE expense_name = 'salaries';
+
+SELECT * FROM employees;
+
+UPDATE employees
+SET hourly_pay=100
+WHERE employee_id='1';
+
+
+SELECT * FROM expenses;
